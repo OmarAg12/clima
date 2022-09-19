62519363537e88a026892a29cda6c21dfd8786b0
OmarAg12 <A01730817@tec.mx>
2022-09-19 17:45:49 -0500

    Mejoramiento de la Api del clima

--- apiClima.js
+++ apiClima.js
@@ -5,14 +5,16 @@ function getWeather() {
 
   var textElement1 = document.getElementById("latitud");
   var textElement2 = document.getElementById("longitud");
-  var resultElement =  document.getElementById("resultado");
+ // var resultElement =  document.getElementById("resultado");
 
   latitud= textElement1.value;
   longitud= textElement2.value;
   $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat="+latitud+"&lon="+longitud+"&appid=9b4f5e18b89f0b63c4398f0afcc702ea",
     function(json) {
-      console.log(json.weather[0].main);
+      console.log(json.weather[0].main),
+      console.log(json.name);
       $('#resultado').text(JSON.stringify(json.weather[0].main));
+      $('#name').text(JSON.stringify(json.name));
     }
   );
 }
